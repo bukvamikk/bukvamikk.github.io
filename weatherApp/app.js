aab2dbbd337d0132734462ba6ca114738f6ad2cf
Miklos Bukva <miki.bukva@gmail.com>
2019-02-04 21:13:26 +0100

    kész

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -4,9 +4,60 @@ window.addEventListener('load', () => {
 
 	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(position => {
-			console.log(position);
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+
+			let temperatureDescription = document.querySelector('.temperature-desciption');
+			let temperatureDegree = document.querySelector('.temperature-degree');
+			let locationTimezone = document.querySelector('.location-timezone');
+			let temperatureSection = document.querySelector('.temperature');
+			const temperatureSpan = document.querySelector('.temperature span');
+
+
+			const proxy = 'https://cors-anywhere.herokuapp.com/';
+			const api = `${proxy}https://api.darksky.net/forecast/e558e3cf9038a0253a2b52e6d39f4c4b/${lat},${long}`;
+
+			fetch(api)
+			.then(response => {
+				return response.json();
+			})
+			.then(data => {
+				console.log(data);
+				const {temperature, summary, icon } = data.currently;
+				// SET DOM ELEMENTS FROM API
+				temperatureDegree.textContent = temperature;
+				temperatureDescription.textContent = summary;
+				locationTimezone.textContent = data.timezone;
+
+			//F to C
+			let celsius =(temperature -32) * (5/9);
+
+			// set icon
+			setIcons(icon, document.querySelector(".icon"));
+
+			//Change temperature 
+				temperatureSection.addEventListener('click',() => {
+				if(temperatureSpan.textContent === "F"){
+					temperatureSpan.textContent = "C";
+					temperatureDegree.textContent= Math.floor(celsius) ;
+				}else{
+					temperatureDegree.textContent = temperature;
+					temperatureSpan.textContent = "F";
+				}
+
+			});
+
+
+
+			});
 		});
-	}else{
-		h1.textContent =  "Hey this is not working because of reasons...";
+	}
+
+	function setIcons(icon, iconID) {
+		  const skycons = new Skycons({"color": "white"});
+		  const currentIcon = icon.replace(/-/g,"_").toUpperCase();
+		  skycons.play();
+		  return skycons.set(iconID, Skycons[currentIcon]);
+
 	}
 });
