e1c736d886f0c97560e6bac9bf1007e3c4d3a6af
Miklos Bukva <miki.bukva@gmail.com>
2019-02-11 13:57:56 +0100

    end

--- memoryGame/scripts.js
+++ memoryGame/scripts.js
@@ -1,7 +1,55 @@
 const cards = document.querySelectorAll('.memory-card');
 
-function flipCard(){
-  //console.log(this);
-  this.classList.toggle('flip');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+	if (lockBoard) return;
+  if(this===firstCard) return; //double click prevent
+	this.classList.add('flip');
+	if (!hasFlippedCard) {
+		//first click
+		hasFlippedCard = true;
+		firstCard = this;
+		return;
+	}
+	//second click
+	hasFlippedCard = false;
+	secondCard = this;
+	checkForMatch();
 }
-cards.forEach(card => card.addEventListener('click',flipCard));
+
+function checkForMatch() {
+	let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+	//cards match
+	isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+	firstCard.removeEventListener('click', flipCard);
+	secondCard.removeEventListener('click', flipCard);
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard=true;
+	setTimeout(() => {
+		firstCard.classList.remove('flip');
+		secondCard.classList.remove('flip');
+    resetBoard();
+	}, 1500);
+}
+function resetBoard(){
+  [hasFlippedCard,lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+  cards.forEach(card =>{
+    let randomPos = Math.floor(Math.random()*12);
+    card.style.order=randomPos;
+  });
+})(); // Immediately Invoked Func. Expression
+
+cards.forEach(card => card.addEventListener('click', flipCard));
